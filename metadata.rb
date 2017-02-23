cf2a4e2c041a074c305a443f5c97818f7047f7b7
Justin Spies <justin.spies@buyerquest.com>
2017-02-23 16:32:03 -0500

    Fix incorrect parameter sequence

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "BuyerQuest, Inc."
 maintainer_email  "itops@buyerquest.com"
 license           "Apache 2.0"
 description       "Installs supervisor and provides resources to configure services; this is a fork of the supervisor cookbook from Noah Kantrowitz <noah@coderanger.net> which seems to no longer be maintained"
-version           "0.5.0"
+version           "1.0.0"
 
 recipe "supervisor", "Installs and configures supervisord"
 
